bb76a32fff4fbc3d2b05533ae8caed891662b83d
JuliusLongmind <5893405+JuliusLongmind@users.noreply.github.com>
2024-10-23 21:00:30 +0200

    Some linting and USE param fix

--- scripts/gentoo/emerge.sh
+++ scripts/gentoo/emerge.sh
@@ -12,7 +12,7 @@ emerge --oneshot sys-apps/portage
 emerge --ask=n --autounmask-write=y --autounmask-continue=y --update --deep --newuse --with-bdeps=y @system @world
 
 # Useful tools.
-USE="-X pam_ssh tty-helpers openssl" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-admin/rsyslog app-admin/sudo app-admin/sysstat app-arch/bzip2 app-arch/bzip2 app-arch/gzip app-arch/gzip app-arch/tar app-arch/xz-utils app-editors/vim app-shells/bash dev-libs/lzo net-misc/curl net-misc/iputils net-misc/openssh net-misc/wget sys-apps/baselayout sys-apps/coreutils sys-apps/diffutils sys-apps/file sys-apps/findutils sys-apps/gawk sys-apps/grep sys-apps/kbd sys-apps/less sys-apps/lm-sensors sys-apps/man-pages sys-apps/mlocate sys-apps/net-tools sys-apps/openrc sys-apps/sed sys-apps/util-linux sys-apps/which sys-apps/which sys-auth/pambase sys-auth/pam_ssh sys-libs/glibc sys-libs/pam sys-process/lsof sys-process/procps sys-process/psmisc net-misc/rsync
+USE="-X pam_ssh tty-helpers openssl" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-admin/rsyslog app-admin/sudo app-admin/sysstat app-arch/bzip2 app-arch/bzip2 app-arch/gzip app-arch/gzip app-arch/tar app-arch/xz-utils app-editors/vim app-shells/bash dev-libs/lzo net-misc/curl net-misc/iputils net-misc/openssh net-misc/wget sys-apps/baselayout sys-apps/coreutils sys-apps/diffutils sys-apps/file sys-apps/findutils sys-apps/gawk sys-apps/grep sys-apps/kbd sys-apps/less sys-apps/lm-sensors sys-apps/man-pages sys-apps/mlocate sys-apps/net-tools sys-apps/openrc sys-apps/sed sys-apps/util-linux sys-apps/which sys-auth/pambase sys-libs/glibc sys-libs/pam sys-process/lsof sys-process/procps sys-process/psmisc net-misc/rsync
 
 # Perform any configuration file updates.
 etc-update --automode -5
@@ -28,7 +28,7 @@ eselect news read --quiet
 emerge --depclean
 
 # Setup vim as the default editor.
-printf "alias vi=vim\n" >> /etc/profile.d/vim.sh
+printf "alias vi=vim\n" >>/etc/profile.d/vim.sh
 
 # Configure the lm_sensors.conf file.
 /usr/sbin/sensors-detect --auto
@@ -50,6 +50,8 @@ fi
 # Create an initial mlocate database.
 updatedb
 
-( sleep 60 ; reboot ) &
+(
+  sleep 60
+  reboot
+) &
 exit 0
-
