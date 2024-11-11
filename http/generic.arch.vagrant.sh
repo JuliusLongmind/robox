222fdfac965275cd9d5789eac241b3c6db986640
JuliusLongmind <5893405+JuliusLongmind@users.noreply.github.com>
2024-11-11 21:54:41 +0100

    Fedora 41 support has been added

--- http/generic.arch.vagrant.sh
+++ http/generic.arch.vagrant.sh
@@ -21,11 +21,11 @@ mount "${device}2" /mnt
 
 # Ensure the leaseweb.net and kernel.org mirrors are always listed, so things work, even when the archlinux
 # website goes offline.
-printf "Server = https://mirror.leaseweb.net/archlinux/\$repo/os/\$arch\n" > /etc/pacman.d/mirrorlist
-printf "Server = https://mirrors.edge.kernel.org/archlinux/\$repo/os/\$arch\n" >> /etc/pacman.d/mirrorlist
+curl -LfsS "https://archlinux.org/mirrorlist/?country=HU" >/tmp/mirrolist
+grep '^#Server' /tmp/mirrolist | grep "https" | sort -R | head -n 5 | sed 's/^#//' >/etc/pacman.d/mirrorlist
 
-curl -LfsS "https://archlinux.org/mirrorlist/?country=all" > /tmp/mirrolist
-grep '^#Server' /tmp/mirrolist | grep "https" | sort -R | head -n 5 | sed 's/^#//' >> /etc/pacman.d/mirrorlist
+printf "Server = https://mirror.leaseweb.net/archlinux/\$repo/os/\$arch\n" >>/etc/pacman.d/mirrorlist
+printf "Server = https://mirrors.edge.kernel.org/archlinux/\$repo/os/\$arch\n" >>/etc/pacman.d/mirrorlist
 
 # Update the keyring and install the initial set of packages required to bootstrap a system.
 killall gpg-agent || exit 1
@@ -36,7 +36,7 @@ pacman --sync --noconfirm --refresh archlinux-keyring || exit 1
 pacstrap /mnt base grub bash sudo linux dhcpcd mkinitcpio openssh || exit 1
 
 swapon "${device}1"
-genfstab -U -p /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt >>/mnt/etc/fstab
 arch-chroot /mnt /bin/bash || exit 1
 [ -f /mnt/etc/fstab.pacnew ] && rm -f /mnt/etc/fstab.pacnew
 swapoff "${device}1"
