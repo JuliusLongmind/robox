4048e3f47eccc5816c9438444d4f3cdbc1d16f59
JuliusLongmind <5893405+JuliusLongmind@users.noreply.github.com>
2024-12-02 22:45:22 +0100

    Some linting

--- scripts/fedora41/dnf.sh
+++ scripts/fedora41/dnf.sh
@@ -6,9 +6,9 @@ retry() {
   local RESULT=0
   while [[ "${COUNT}" -le 10 ]]; do
     [[ "${RESULT}" -ne 0 ]] && {
-      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+      [ "$(which tput 2>/dev/null)" != "" ] && [ -n "$TERM" ] && tput setaf 1
       echo -e "\n${*} failed... retrying ${COUNT} of 10.\n" >&2
-      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+      [ "$(which tput 2>/dev/null)" != "" ] && [ -n "$TERM" ] && tput sgr0
     }
     "${@}" && { RESULT=0 && break; } || RESULT="${?}"
     COUNT="$((COUNT + 1))"
@@ -19,23 +19,23 @@ retry() {
   done
 
   [[ "${COUNT}" -gt 10 ]] && {
-    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+    [ "$(which tput 2>/dev/null)" != "" ] && [ -n "$TERM" ] && tput setaf 1
     echo -e "\nThe command failed 10 times.\n" >&2
-    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+    [ "$(which tput 2>/dev/null)" != "" ] && [ -n "$TERM" ] && tput sgr0
   }
 
   return "${RESULT}"
 }
 
 error() {
-        if [ $? -ne 0 ]; then
-                printf "\n\ndnf failed...\n\n";
-                exit 1
-        fi
+  if [ $? -ne 0 ]; then
+    printf "\n\ndnf failed...\n\n"
+    exit 1
+  fi
 }
 
 # Tell dnf to retry 128 times before failing, so unattended installs don't skip packages when errors occur.
-printf "\nretries=128\ndeltarpm=false\nmetadata_expire=300\ntimeout=300\n" >> /etc/dnf/dnf.conf
+printf "\nretries=128\ndeltarpm=false\nmetadata_expire=300\ntimeout=300\n" >>/etc/dnf/dnf.conf
 
 # Disable the subscription manager plugin.
 if [ -f /etc/yum/pluginconf.d/subscription-manager.conf ]; then
@@ -51,28 +51,35 @@ fi
 sysctl net.ipv6.conf.all.disable_ipv6=1
 
 # Update the base install first.
-retry dnf upgrade --assumeyes; error
+retry dnf upgrade --assumeyes
+error
 
 # Needed to retrieve source code, and other misc system tools.
-retry dnf install --assumeyes vim git wget curl rsync gnupg sysstat lsof pciutils usbutils psmisc; error
+retry dnf install --assumeyes vim git wget curl rsync gnupg sysstat lsof pciutils usbutils psmisc
+error
 
 # Packages needed beyond a minimal install to build and run magma.
-retry dnf install --assumeyes valgrind valgrind-devel texinfo autoconf automake libtool ncurses-devel gcc-c++ libstdc++-devel gcc cpp glibc-devel glibc-headers kernel-headers mpfr ppl perl perl-Module-Pluggable perl-Pod-Escapes perl-Pod-Simple perl-libs perl-version patch sysstat perl-Time-HiRes make cmake libarchive deltarpm net-tools; error
+retry dnf install --assumeyes valgrind valgrind-devel texinfo autoconf automake libtool ncurses-devel gcc-c++ libstdc++-devel gcc cpp glibc-devel glibc-headers kernel-headers mpfr ppl perl perl-Module-Pluggable perl-Pod-Escapes perl-Pod-Simple perl-libs perl-version patch sysstat perl-Time-HiRes make cmake libarchive deltarpm net-tools python3-libdnf5
+error
 
 # Grab the required packages from the EPEL repo.
-retry dnf install --assumeyes libbsd libbsd-devel; error
+retry dnf install --assumeyes libbsd libbsd-devel
+error
 systemctl restart NetworkManager
 
 # Boosts the available entropy which allows magma to start faster.
-retry dnf install --assumeyes haveged; error
+retry dnf install --assumeyes haveged
+error
 systemctl restart NetworkManager
 
 # The daemon services magma relies upon.
-retry dnf install --assumeyes libevent memcached; error
+retry dnf install --assumeyes libevent memcached
+error
 systemctl restart NetworkManager
 
 # Packages used to retrieve the magma code, but aren't required for building/running the daemon.
-retry dnf install --assumeyes wget git rsync perl-Git perl-Error; error
+retry dnf install --assumeyes wget git rsync perl-Git perl-Error
+error
 systemctl restart NetworkManager
 
 # These packages are required for the stacie.py script, which requires the python cryptography package (installed via pip).
@@ -80,15 +87,16 @@ retry dnf install --assumeyes python-crypto python-cryptography
 systemctl restart NetworkManager
 
 # Packages used during the provisioning process and then removed during the cleanup stage.
-retry dnf install --assumeyes sudo dmidecode; error
+retry dnf install --assumeyes sudo dmidecode
+error
 systemctl restart NetworkManager
 
 # Run update a second time, just in case it failed the first time. Mirror timeouts and cosmic rays
 # often interrupt the the provisioning process.
-retry dnf upgrade --assumeyes; error
+retry dnf upgrade --assumeyes
+error
 systemctl restart NetworkManager
 
 # Reboot onto the new kernel (if applicable).
-( shutdown --reboot --no-wall +1 ) &
+(shutdown --reboot --no-wall +1) &
 exit 0
-
