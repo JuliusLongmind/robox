c63fc8ff6a83462b65f7b789aa795d4cca4295fb
JuliusLongmind <5893405+JuliusLongmind@users.noreply.github.com>
2025-05-29 22:58:57 +0200

    Dealing with vagrant networking issue

--- scripts/tumbleweed/network.sh
+++ scripts/tumbleweed/network.sh
@@ -4,3 +4,8 @@
 printf "Cleaning up udev rules.\n"
 rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
+
+zypper --non-interactive install --force-resolution wicked dhcpcd
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+systemctl enable wicked --now
